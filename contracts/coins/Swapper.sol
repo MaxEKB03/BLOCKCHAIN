f3e6f2dd9b2ac159e54fdb30c099947cd4d5c3fb
Maksim Kuznetsov <maxekb@live.ru>
2022-04-26 11:13:56 +0300

    Swapps add to repository

--- contracts/coins/Swapper.sol
+++ contracts/coins/Swapper.sol
@@ -6,11 +6,12 @@ import "../interfaces/IERC20.sol";
 contract Swapper {
     
     struct Swap{
-        address onwer1;
-        address onwer2;
+        address owner1;
+        address owner2;
         mapping (address => IERC20) token;
         mapping (address => bool) approve;
         mapping (address => bool) participate;
+        mapping (address => uint) amount;
     }
 
     mapping (uint=>Swap) public swaps;
@@ -22,11 +23,16 @@ contract Swapper {
         _;
     }
 
-    function InitSwap(address onwer1, address onwer2, address token1, address token2) public {
-        token[onwer1] = IERC20(token1);
-        token[onwer2] = IERC20(token2);
-        swaps[swapsCount] = Swap(onwer1, onwer2, token, IERC20(token2));
-        swapsCount ++;
+    function InitSwap(address owner1, address owner2, address token1, address token2, uint amount1, uint amount2) public {
+        swaps[swapsCount].owner1 = owner1;
+        swaps[swapsCount].owner2 = owner2;
+        swaps[swapsCount].token[owner1] = IERC20(token1);
+        swaps[swapsCount].token[owner2] = IERC20(token2);
+        swaps[swapsCount].participate[owner1] = true;
+        swaps[swapsCount].participate[owner2] = true;
+        swaps[swapsCount].amount[owner1] = amount1;
+        swaps[swapsCount].amount[owner2] = amount2;
+        swapsCount++;
     }
 
     function DeleteSwap(uint swapId) public CheckHolding(swapId){
@@ -34,13 +40,24 @@ contract Swapper {
     }
 
     function Approve(uint swapId) public CheckHolding(swapId){
+        swaps[swapId].approve[msg.sender] = true;
     }
 
     function Deligate(uint swapId) public CheckHolding(swapId){
         address owner1 = swaps[swapId].owner1;
         address owner2 = swaps[swapId].owner2;
-        IERC20 token1 = swaps[swapId].token1;
-        IERC20 token2 = swaps[swapId].token2;
+        IERC20 token1 = swaps[swapId].token[owner1];
+        IERC20 token2 = swaps[swapId].token[owner2];
+        uint amount1 = swaps[swapId].amount[owner1];
+        uint amount2 = swaps[swapId].amount[owner2];
 
+        require(token1.allowance(owner1, address(this)) >= amount1, "Owner1's allowance is lower than sad");
+        require(token2.allowance(owner2, address(this)) >= amount2, "Owner2's allowance is lower than sad");
+
+        token1.transferFrom(owner1, address(this), amount1);
+        token2.transferFrom(owner2, address(this), amount2);
+
+        token1.transfer(owner2, amount1);
+        token2.transfer(owner1, amount2);
     }
 }
